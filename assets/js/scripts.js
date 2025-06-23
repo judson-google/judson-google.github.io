3787e0de13cfd43e35e4d8fa97c8759a063399ff
Judson Gabriel <judsongabrielfdossantos@gmail.com>
2025-06-22 20:07:58 -0400

    init

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector("#btn-menu")
+
+menu.addEventListener("click", () => {
+
+    console.log("clicou mona")
+
+
+})
+
+const sorte = document.querySelector("#btn-tres")
+
+sorte.addEventListener("click", () => {
+
+    location.href = "https://www.youtube.com"
+
+})
